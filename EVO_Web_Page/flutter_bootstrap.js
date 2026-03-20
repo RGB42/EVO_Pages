b6cba9b9207dbfafecfe6e25ba752404095452eb
RGB42 <40913227+RGB42@users.noreply.github.com>
2026-03-20 11:41:06 +0000

    Deploying to main from @ RGB42/EVO@60bf96c1de4e7d8a70f04bc7544948a22d6d7cfa 🚀

--- EVO_Web_Page/flutter_bootstrap.js
+++ EVO_Web_Page/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a5cb96369ef86c7e85abf5d662a1ca5d897750
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3433691238"
+    serviceWorkerVersion: "2893466086"
   }
 });
